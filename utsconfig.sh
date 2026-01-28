be9b275a532738a6f9f885a3a4cbae22ee2c5192
agadsby <andrew@gadsby.me.uk>
2026-01-28 10:00:29 +0000

    Removed need for UTS DIRECT cards

--- utsconfig.sh
+++ utsconfig.sh
@@ -6,22 +6,21 @@
 TOP="${VMHOME:-$HOME/VM}"
 cd $TOP
 echo "Start VM370 using a another session"
-echo -n "Enter when done : "
+echo -n "Hit <Enter> when done : "
 read line
 echo "Enabling spool devices on VM370"
 vmcmd "/start all"
 sleep 1
 echo "Send UTS Config Cards to MAINT via RDR"
 vmrdr -v MAINT cards/adduts_exec.cards
-sleep 2
-vmrdr -v MAINT cards/uts_direct.cards	
-sleep 2
+sleep 1
 echo "Loading UTS installation tape on 0480"
 vmcmd devinit 0480 tapes/Amdahl_UTS2.aws
 sleep 1
-echo "Using a 3270 window LOGIN MAINT password CPCMS"
-echo "enter READCARD ADDUTS EXEC"
-echo "enter ADDUTS"
+echo "Using a 3270 window\
+LOGIN MAINT CPCMS\
+READCARD ADDUTS EXEC \
+ADDUTS"
 echo "Once succesfully run use another 3270 window to DIAL UTS"
 echo "login using root / root"
 echo "To shutdown use 'vmcmd / shutdown' followed by 'vmcmd exit'"
